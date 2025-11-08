112631b7891d5e8b1ab51bc6868887c5dea666fb
wojciech-san <wojciech.czok@gmail.com>
2025-11-08 16:35:38 +0000

    updated docker file to run as  fastpi

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,17 @@
 FROM python:3.12-slim
 
-
 RUN pip install pipenv
-
 
 WORKDIR /app
 
-
 COPY ["Pipfile", "Pipfile.lock", "./"]
-
-
 RUN pipenv install --system --deploy
 
 COPY ["predict.py", "./"]
-
-
+ 
 COPY ["models/random_forest_heart_disease_v1.bin", "./models/random_forest_heart_disease_v1.bin"]
-
 
 EXPOSE 9696
 
+ENTRYPOINT ["uvicorn", "predict:app", "--host", "0.0.0.0", "--port", "9696"]
 
-ENTRYPOINT ["gunicorn", "--bind=0.0.0.0:9696", "predict:app"]
--- predict.py
+++ predict.py
@@ -1,38 +1,43 @@
-import pickle
-from flask import Flask
-from flask import request
-from flask import jsonify
-
+from fastapi import FastAPI
+from pydantic import BaseModel
 import os
+import pickle
 
-model_file = os.path.join(os.path.dirname(__file__), "models", "random_forest_heart_disease_v1.bin")
+class PatientData(BaseModel):
+    age: float
+    sex: str
+    cp: str
+    trestbps: float
+    chol: float
+    fbs: str
+    restecg: str
+    thalch : float
+    exang: str
+    oldpeak: float
+    slope: str
+    ca: int
+    thal: str
 
-#model_file = '/workspaces/heart-disease-prediction-system/models/random_forest_heart_disease_v1.bin'
+app = FastAPI(title="Heart Disease Prediction API")
+
+
+model_path = os.path.join(os.path.dirname(__file__), "models", "random_forest_heart_disease_v1.bin")
+with open(model_path, "rb") as f_in:
+    dv, model = pickle.load(f_in)  
+
+@app.post("/predict")
+def predict(data: PatientData):
  
-with open(model_file, 'rb') as f_in:
-    dv, model = pickle.load(f_in)
- 
-app = Flask('heart_disease')
- 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # json = Python dictionary
-    patient = request.get_json()
- 
-    X = dv.transform([patient])
-    y_pred = model.predict_proba(X)[0,1] 
-    heart_disease = y_pred >= 0.5
- 
-    result = {
-        'heart_disease_probability': float(y_pred),
-        'heart_disease': bool(heart_disease)
+    data_dict = data.dict()
+    
+
+    X = dv.transform([data_dict])
+    
+  
+    pred_proba = model.predict_proba(X)[0, 1]
+    pred_class = int(model.predict(X)[0])
+    
+    return {
+        "prediction": pred_class,
+        "probability": float(pred_proba)
     }
- 
-    return jsonify(result)
-
-@app.route('/health', methods=['GET'])
-def health():
-    return jsonify({'status': 'healthy', 'model_loaded': True})
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9696)
